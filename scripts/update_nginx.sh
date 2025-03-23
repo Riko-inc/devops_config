7123030b34bc5412816f6e33b8ac246a9cf3aa7d
Maxim <therikomero@gmail.com>
2025-03-24 01:09:13 +0700

    config fixes

---
+++ scripts/update_nginx.sh
@@ -0,0 +1,5 @@
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+  -n services \
+  -f /home/user1/common/nginx-values.yaml
+
+kubectl apply -f /home/user1/task-service/ingress.yaml
